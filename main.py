16dc6e4194d2f2de2400f1e00a371ab017d8669f
smorgenfeld <smorgenfeld@users.noreply.github.com>
2015-12-27 20:21:30 -0800

    yyayyay

--- main.py
+++ main.py
@@ -11,14 +11,14 @@ def main(win,t_height,t_type,t_biome,land,w,h):
         keey = win.getKey()
         if keey == 'h':
             cont = 0
-            tmod += 3
+            tmod += 1
             for i in range(h):
                 for j in range(w):
                     t_height[i][j],t_type[i][j],t_biome[i][j] = terrain.changeter(t_height[i][j],t_type[i][j],tmod,0,land,cont,i,w,h)
                     cont += 1
         elif keey == 'c':
             cont = 0
-            tmod -= 3
+            tmod -=1
             for i in range(h):
                 for j in range(w):
                     t_height[i][j],t_type[i][j],t_biome[i][j] = terrain.changeter(t_height[i][j],t_type[i][j],tmod,0,land,cont,i,w,h)
@@ -26,21 +26,22 @@ def main(win,t_height,t_type,t_biome,land,w,h):
         elif keey == 'q':
             win.quit()
             sys.exit()
-        yay = terrain.getTiles(t_biome,h)
-        difyay = ((yay[0]/10+yay[2]/20+yay[4]/30+yay[6]/10+yay[11]/3+yay[16])-iceyyay)/2000
-        print(difyay)
-        cont = 0
-        for i in range(h):
-            for j in range(w):
-                t_height[i][j],t_type[i][j],t_biome[i][j] = terrain.changeter(t_height[i][j],t_type[i][j],tmod,difyay,land,cont,i,w,h)
-                cont += 1
-        win.update()
-        yay = terrain.getTiles(t_biome,h)
-        print(yay)
+        if keey == 'h' or keey == 'c':
+            yay = terrain.getTiles(t_biome,h)
+            difyay = ((yay[0]/10+yay[2]/20+yay[4]/30+yay[6]/10+yay[11]/3+yay[16])-iceyyay)/2000
+            print(difyay)
+            cont = 0
+            for i in range(h):
+                for j in range(w):
+                    t_height[i][j],t_type[i][j],t_biome[i][j] = terrain.changeter(t_height[i][j],t_type[i][j],tmod,difyay,land,cont,i,w,h)
+                    cont += 1
+            win.update()
+            yay = terrain.getTiles(t_biome,h)
+            print(yay)
 def start():
     w=192
     h=108
-    win = GraphWin('game4',w*10,h*10,fullscreen=True,autoflush=False)
+    win = GraphWin('game4',w*10,h*10,fullscreen=False,autoflush=False)
     win.setCoords(0,0,w,h)
     loading = Text(Point(w/2,h/2),'Generating Map...');loading.setSize(36);loading.draw(win)
     win.update()
